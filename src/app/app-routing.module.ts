08dcde911120a7e510f4be199438d02ed8cdacf8
unknown <sanjayselvaraj470@gmail.com>
2022-01-16 14:23:38 +0530

    added header,routing and other links in header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ExpComponent } from './exp/exp.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes= [
-  {path:'exp', component:ExpComponent}
+  {path:'contactus', component:ExpComponent},
+  {path:'home', component:HomeComponent}
  ]
 @NgModule({
   declarations: [],
