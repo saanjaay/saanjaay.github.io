0f3b88585e79f163f24d55cdb776aa1a952c53b3
Sanjay Selvaraj <sanjay.selvaraj@accenture.com>
2022-05-28 23:01:24 +0530

    removed old components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ExpComponent } from './exp/exp.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
-import { GetcoursesComponent } from './getcourses/getcourses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExpComponent,
-    HomeComponent,
-    GetcoursesComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
