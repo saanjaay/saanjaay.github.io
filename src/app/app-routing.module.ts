39136daffa69a282b78a12adc8e4a4a58ee55818
Sanjay Selvaraj <sanjay.selvaraj@accenture.com>
2022-05-30 17:46:00 +0530

    header changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ExpComponent } from './exp/exp.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { GetcoursesComponent } from './getcourses/getcourses.component';
 
 const routes: Routes= [
-  {path:'contactus', component:ExpComponent},
-  {path:'home', component:HomeComponent},
-  {path:'getcourses',component:GetcoursesComponent}
+  {path:'home', component:HomeComponent}
  ]
 @NgModule({
   declarations: [],
